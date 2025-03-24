f88e92157f281811c6084f487d7e0296fc4f20e0
Garik Asplund <garik.asplund@gmail.com>
2025-03-24 11:23:23 -0700

    added algolia env vars

--- src/lib/components/Comparisons.svelte
+++ src/lib/components/Comparisons.svelte
@@ -1,23 +1,27 @@
 <script>
   import DotPattern from "./UI/DotPattern.svelte";
   import YouTubeTalk from "./YouTubeTalk.svelte";
+  import FlipText from "./UI/FlipText.svelte";
 </script>
 
-<section class="relative w-full flex flex-col items-center transform-gpu will-change-transform">
+<section
+  class="relative w-full flex flex-col items-center transform-gpu will-change-transform"
+>
   <div class="absolute inset-0 bg-black">
     <div
       class="absolute inset-0 bg-gradient-to-b from-black to-blue-950 opacity-90"
     ></div>
     <div class="absolute inset-0 backdrop-blur-[2px]"></div>
   </div>
-  
+
   <div
     class="relative flex h-48 w-full items-center justify-center overflow-hidden bg-none"
   >
-    <h2
-      class=       "font-display text-center text-2xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-4xl md:leading-[5rem]"
-    >
-      Serverless functions with speed <i>and</i> security
+    <h2>
+      <FlipText
+      class="font-display text-center text-2xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-4xl md:leading-[5rem]"
+      word="Serverless functions with speed and security"
+      />
     </h2>
     <DotPattern
       class="[mask-image:linear-gradient(white,transparent)] opacity-80"
@@ -30,6 +34,6 @@
       cx={2}
     />
   </div>
-  
+
   <YouTubeTalk />
 </section>
--- vite.config.ts
+++ vite.config.ts
@@ -16,23 +16,15 @@ const config = defineConfig({
         themeColor: {
           light: "#fff",
           dark: "#000",
-          // hover: "#7d2fcc",
-          // primary: "#3e1766",
-          // primary: "#818CF8",
-          // hover: "#D8B4FE",
-          // primary: "#8A7CFF", // Matches the HYPERLIGHT text color
-          // hover: "#9A4EFC", // Matches the documentation button
-          // primary: "#4F46E5", // Dark Indigo - excellent contrast on white
-          // hover: "#7C3AED", // Rich Purple - good secondary option
           gradient: {
             start: "#D8B4FE",
             end: "#818CF8",
           },
         },
         docsearch: {
-          appId: "SSS54K4ITB",
-          apiKey: "ca1cf8479bb21819f3d5f9deaf23b7ad",
-          indexName: "hyperlight_draft_vercel_app_sss54k4itb_pages",
+          appId: import.meta.env.VITE_ALGOLIA_APP_ID,
+          apiKey: import.meta.env.VITE_ALGOLIA_API_KEY,
+          indexName: import.meta.env.VITE_ALGOLIA_INDEX_NAME,
         },
         navbar,
         sidebar,
