3510a804e1dc5b0540ec7b18b5db72197cb20edc
Garik Asplund <garik.asplund@gmail.com>
2025-03-23 18:48:09 -0700

    nowrap on blogpost sidebar, better lineheight

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,6 +1,14 @@
-<style>
-  :global(.sidebar-group .link-wrapper a) {
-    white-space: normal !important;
-    line-height: 1.4;
-  }
-</style>
+<script lang="ts">
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <style>
+    .sidebar-group .link-wrapper a {
+      white-space: normal !important;
+      line-height: 1.4;
+    }
+  </style>
+</svelte:head>
+
+{@render children()}
